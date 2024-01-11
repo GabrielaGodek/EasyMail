e0ef52f67caa7e4d41c1aebf7ea5a25e5346387d
g0gab1s <83614025+GabrielaGodek@users.noreply.github.com>
2024-01-11 17:54:01 +0100

    secure url

--- routes.php
+++ routes.php
@@ -1,8 +1,8 @@
 <?php
 require_once __DIR__ . '/router.php';
 
-// Encode the URL to replace special characters with percent-encoded values
-$dirname = urlencode(dirname($_SERVER["REQUEST_URI"]));
+// Remove possible HTML tags from url
+$dirname = strip_tags(dirname($_SERVER["REQUEST_URI"]));
 
 get($dirname . '/index', '/index.php');
 get($dirname . '/users', '/src/views/users.php');
